93f76cd499c1b9a4c332563734345731954a6d8d
aayush jain <aayushvjain@gofynd.com>
2023-01-03 11:07:00 +0530

    example extension react javascript

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# example-extension-javascript-react
-This is example extension for Javascript and react
+# groot
+Node 14
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### To start react development server
+```
+npm run start
+```
+
+### start server
+```
+npm run serve
+```
---
+++ app/routes/v1.router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/test-api', (req, res, next) => {
+    res.json({
+        "hello": "hello"
+    });
+});
+
+// Get applications list
+router.get('/applications', async function view(req, res, next) {
+    try {
+        const {
+            platformClient
+        } = req;
+        return res.json(await platformClient.configuration.getApplications({
+            pageSize: 1000,
+            q: JSON.stringify({"is_active": true})
+        }));
+    } catch (err) {
+        next(err);
+    }
+});
+
+module.exports = router;
