b6dfc3f03b330f90d3bf0a5191c231358234b2d7
jinalviranii <jinalvirani@gofynd.com>
2024-06-19 15:24:08 +0530

    Change UI for boilerplate

--- app/routes/v1.router.js
+++ app/routes/v1.router.js
@@ -3,16 +3,28 @@ const express = require('express');
 const router = express.Router();
 
 
-// Get applications list
-router.get('/applications', async function view(req, res, next) {
+// Get products list
+router.get('/products', async function view(req, res, next) {
     try {
         const {
             platformClient
         } = req;
-        return res.json(await platformClient.configuration.getApplications({
-            pageSize: 1000,
-            q: JSON.stringify({"is_active": true})
-        }));
+        const data = await platformClient.catalog.getProducts()
+        return res.json(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+// Get products list for application
+router.get('/:application_id/products', async function view(req, res, next) {
+    try {
+        const {
+            platformClient
+        } = req;
+        const { application_id } = req.params;
+        const data = await platformClient.application(application_id).catalog.getAppProducts()
+        return res.json(data);
     } catch (err) {
         next(err);
     }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,50 +1,70 @@
 import React, { useState, useEffect } from "react";
-
+import { useParams } from 'react-router-dom';
 import "./style/home.css";
 import Loader from "../components/Loader";
-import { Input } from "@gofynd/nitrozen-react";
 import MainService from "../services/main-service";
-
+import greenDot from "../assets/green-dot.svg";
+import grayDot from "../assets/grey-dot.svg";
+import isEmpty from "lodash/isEmpty";
+import DEFAULT_NO_IMAGE from "../assets/default_icon_listing.png";
 
 export default function Home() {
   const [pageLoading, setPageLoading] = useState(false);
-  const [applicationList, setApplicationList] = useState([]);
-  const [allApplications, setAllApplications] = useState([]);
-
+  const [product_list, setProductList] = useState([]);
+  const DOC_URL_PATH =
+    "/help/docs/sdk/latest/platform/company/catalog/#getProducts";
+  const DOC_APP_URL_PATH =
+    "/help/docs/sdk/latest/platform/application/catalog#getAppProducts";
+  const { application_id } = useParams();
   useEffect(() => {
-    fetchApplications();
+    isApplicationLaunch() ? fetchApplicationProducts() :
+      fetchProducts();
   }, []);
 
-  const fetchApplications = async () => {
+  const fetchProducts = async () => {
     setPageLoading(true);
     try {
-      const { data } = await MainService.getAllApplications();
-      setAllApplications(data.items);
-      const temp = data.items.map((ele) => {
-        ele.text = ele.name;
-        ele.value = ele._id;
-        ele.image = ele.logo;
-        ele.logo = ele.image && ele.image.secure_url;
-        return ele;
-      });
-      setApplicationList(temp);
+      const { data } = await MainService.getAllProducts();
+      setProductList(data.items);
       setPageLoading(false);
     } catch (e) {
       setPageLoading(false);
     }
   };
 
-  function searchApplication(event) {
-    let searchText = event.target.value;
-    if (!searchText) {
-      setApplicationList(allApplications.map((app) => app));
-    } else {
-      setApplicationList(
-        allApplications.filter((item) => {
-          return item.name.toLowerCase().includes(searchText.toLowerCase());
-        })
-      );
+  const fetchApplicationProducts = async () => {
+
+    setPageLoading(true);
+    try {
+      const { data } = await MainService.getAllApplicationProducts({
+        application_id: application_id
+      })
+      setProductList(data.items);
+      setPageLoading(false);
+    } catch (e) {
+      setPageLoading(false);
     }
+  }
+
+  const productProfileImage = (media) => {
+    if (isEmpty(media)) {
+      return DEFAULT_NO_IMAGE;
+    }
+    const profileImg = media.find((m) => m.type === "image");
+    if (isEmpty(profileImg) || !profileImg.url) {
+      return DEFAULT_NO_IMAGE;
+    }
+    return profileImg.url;
+  };
+
+  const getDocumentPageLink = () => {
+    return "https://api.uat.fyndx1.de"
+      .replace("api", "partners")
+      .concat(isApplicationLaunch() ? DOC_APP_URL_PATH : DOC_URL_PATH);
+  }
+
+  const isApplicationLaunch = () => {
+    return application_id ? true : false;
   }
 
   return (
@@ -52,39 +72,80 @@ export default function Home() {
       {pageLoading ? (
         <Loader />
       ) : (
-        <div className="application-container">
-
-          <div className="saleschannel-title">Sales Channel</div>
-
-          <div className="search-box">
-            <Input
-              showSearchIcon
-              placeholder='search sales channels'
-              disabled={ Object.keys(allApplications).length === 0 ? true : false }
-              onChange={searchApplication}
-            />
+        <div className="products-container">
+          <div class="title">
+            This is an example extension home page user interface.
           </div>
-          
-          <div className="sales-channels-container">
-            {applicationList.map((application) => {
+
+          <div class="section">
+            <div class="heading">
+              <span>Example Platform API</span> :{" "}
+              <a href={getDocumentPageLink()} target="_blank">
+                {isApplicationLaunch() ? 'getAppProducts' : 'getProducts'}
+              </a>
+            </div>
+            <div class="description">
+              This is an illustrative Platform API call to fetch the list of
+              products in this company. Go to your extension folder’s
+              ‘root/app/routes/’ directory to check how to call Platform API and
+              start calling API you require.
+            </div>
+          </div>
+
+          <div>
+            {product_list.map((product, index) => {
               return (
-                  <div className="app-box">
-
-                    <div className="logo">
-                      <img src={application.logo ? application.logo : "https://platform.fynd.com/public/admin/assets/pngs/fynd-store.png"} alt="logo" />
-                    </div>
-
-                    <div className="line-1">{application.name}</div>
-
-                    <div className="line-2">{application.domain.name}</div>
+                <div class="product-list-container flex-row">
+                  {product.is_active ? (
+                    <img class="mr-r-12" src={greenDot} />
+                  ) : (
+                    <img class="mr-r-12" src={grayDot} />
+                  )}
+                  <div class="card-avatar mr-r-12">
+                    <img src={productProfileImage(product.media)} alt="text" />
                   </div>
+                  <div class="flex-column">
+                    <div class="flex-row">
+                      <div class="product-name" id={`product-name-${index}`}>
+                        {product.name}
+                      </div>
+                      <div class="product-item-code">|</div>
+                      {product.item_code && (
+                        <>
+                          <span class="product-item-code">
+                            Item Code:
+                            <span
+                              class="cl-RoyalBlue"
+                              id={`product-item-code-${index}`}
+                            >
+                              {product.item_code}
+                            </span>
+                          </span>
+                        </>
+                      )}
+                    </div>
+                    {product.brand && (
+                      <div
+                        class="product-brand-name"
+                        id={`product-brand-name-${index}`}
+                      >
+                        {product.brand.name}
+                      </div>
+                    )}
+                    {product.category_slug && (
+                      <div
+                        class="product-brand-name"
+                        id={`product-category-slug-${index}`}
+                      >
+                        Category <span>: </span>{" "}
+                        <span>{product.category_slug}</span>
+                      </div>
+                    )}
+                  </div>
+                </div>
               );
             })}
-            {applicationList.length % 3 === 2 && (
-              <div className="app-box hidden"></div>
-            )}
           </div>
-          
         </div>
       )}
     </>
