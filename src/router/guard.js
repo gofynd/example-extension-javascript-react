5ab10c02965464e7c0611d2136f3a5171e889a2b
jinalviranii <jinalvirani@gofynd.com>
2024-07-10 14:22:46 +0530

    Fix syntax issue

--- src/router/guard.js
+++ src/router/guard.js
@@ -1,12 +1,11 @@
 import { setCompany, setApplication } from '../helper/utils';
 
-export const routeGuard = (to, from, next) => {
-  const { params } = to;
+export const routeGuard = ({params}) => {
   if (params.company_id) {
     setCompany(params.company_id);
   }
   if (params.application_id) {
     setApplication(params.application_id);
   }
-  next();
+  return null;
 };
