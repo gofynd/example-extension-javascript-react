1b2fdc810a69403db0a65b9275802f6fbbfc3630
Bhargav Prajapati <bhargavprajapati@gofynd.com>
2024-07-16 18:24:48 +0530

    Changed default port for extension server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("./app/common/redis.init");
 const config = require("./app/config");
 const app = require("./app/server");
 
-const port = config.port || 3000;
+const port = config.port || 8080;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
