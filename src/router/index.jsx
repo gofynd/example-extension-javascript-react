051e91d70504047e54a153ed2cf225a4bc752a6f
vivek-gofynd <vivekprajapati@fynd.com>
2024-07-29 12:58:35 +0530

    HMR support

---
+++ src/router/index.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router-dom";
+import { routeGuard } from "./guard";
+
+import App from "../App";
+
+const router = createBrowserRouter([
+  {
+    path: "/company/:company_id/",
+    element: <App />,
+    loader: routeGuard,
+  },
+  {
+    path: "/company/:company_id/application/:application_id",
+    element: <App />,
+    loader: routeGuard,
+  },
+]);
+
+export default router;
